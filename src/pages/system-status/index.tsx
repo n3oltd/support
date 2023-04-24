d29bd1bee04760fd3c3dd4ab7cb89463c1afb0ad
Noor Drummond <noor.drummond@n3o.ltd>
2023-04-24 15:29:42 +0100

    Fix function name

--- src/pages/system-status/index.tsx
+++ src/pages/system-status/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 
-export default function Videos(): JSX.Element {
+export default function Content(): JSX.Element {
     return (
         <Layout
             title="System Status"
