a92e1f31d236e78cd6cbee2c9917685b372ace24
rn3o <98315488+rn3o@users.noreply.github.com>
2023-04-24 16:51:00 +0100

    remove extra white space from iframe content

--- src/pages/roadmap-ideas-board/index.tsx
+++ src/pages/roadmap-ideas-board/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 
+const style = {
+    "minHeight" : "100vh"
+}
+
 export default function Content(): JSX.Element {
     return (
         <Layout
             title="Roadmap & Ideas Board"
             description="View the status of planned product features and submit new ideas">
-            <iframe src="https://app.loopedin.io/n3o" height="900" width="100%" frameBorder="0"></iframe>
+            <iframe src="https://app.loopedin.io/n3o" style={style} width="100%" frameBorder="0"></iframe>
         </Layout>
     );
 }
--- src/pages/system-status/index.tsx
+++ src/pages/system-status/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 
+const style = {
+    "minHeight" : "100vh"
+}
+
 export default function Videos(): JSX.Element {
     return (
         <Layout
             title="System Status"
             description="View details of any upcoming maintenance or incidents in progress">
-            <iframe src="https://status.n3o.ltd" height="900" width="100%" frameBorder="0"></iframe>
+            <iframe src="https://status.n3o.ltd" style={style} width="100%" frameBorder="0"></iframe>
         </Layout>
     );
 }
